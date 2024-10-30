6e96f3baa2af2283c26f4b85bb5a4be97db81415
Rai <lynx7478@gmail.com>
2024-10-30 10:42:29 +0100

    Create Result enum

---
+++ src/main/java/com/gmail/lynx7478/vFurnace/Result.java
@@ -0,0 +1,42 @@
+package com.gmail.lynx7478.vFurnace;
+
+import org.bukkit.Material;
+
+public enum Result
+{
+
+    IRON(Material.IRON_ORE, Material.IRON_INGOT);
+
+    public static Result getByMaterial(Material m)
+    {
+        for(Result r : Result.values())
+        {
+            if(r.getMaterial() == m)
+            {
+                return r;
+            }
+        }
+
+        return null;
+    }
+
+    private Material mat;
+    private Material result;
+
+    Result(Material mat, Material result)
+    {
+        this.mat = mat;
+        this.result = result;
+    }
+
+    public Material getMaterial()
+    {
+        return mat;
+    }
+
+    public Material getResult()
+    {
+        return result;
+    }
+
+}
